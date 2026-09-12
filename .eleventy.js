352fe2bf8405e43ae680a45441c938214fe28efb
gef67600 <gef67600@gmail.com>
2026-09-12 16:38:34 +0200

    saisie1

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("content/**/*.{png,jpg,jpeg,webp}");
   eleventyConfig.ignores.add("content/Templates/**");
   eleventyConfig.addPassthroughCopy("content/style.css");
+  eleventyConfig.addCollection("tueurs", (api) => api.getFilteredByGlob("content/Tueurs/*.md"));
+  eleventyConfig.addCollection("mafieux", (api) => api.getFilteredByGlob("content/Mafieux/*.md"));
+  eleventyConfig.addCollection("escrocs", (api) => api.getFilteredByGlob("content/Escrocs/*.md"));
 
   return {
     dir: {
